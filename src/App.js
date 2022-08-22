b22ad94313ed1542cdedc1c4582bbcb2ae27783b
Abishek-Lakshmanan <abishekshark@gmail.com>
2022-08-23 00:22:41 +0530

    countDown Timer added with functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Clock from './Components/Clock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [timerDays, setTimerDays] = useState();
+	const [timerHours, setTimerHours] = useState();
+	const [timerMinutes, setTimerMinutes] = useState();
+	const [timerSeconds, setTimerSeconds] = useState();
+
+	let interval;
+
+	const startTimer = () => {
+		const countDownDate = new Date('Jan 5, 2023').getTime();
+
+		// Update the count down every 1 second
+		interval = setInterval(() => {
+			// Get today's date and time
+			const now = new Date().getTime();
+
+			// Find the distance between now and the count down date
+			const distance = countDownDate - now;
+
+			// Time calculations for days, hours, minutes and seconds
+			const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			const hours = Math.floor(
+				(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+			);
+			const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(interval.current);
+			} else {
+				setTimerDays(days);
+				setTimerHours(hours);
+				setTimerMinutes(minutes);
+				setTimerSeconds(seconds);
+			}
+		});
+	};
+
+	useEffect(() => {
+		startTimer();
+	});
+
+	return (
+		<div className="App">
+			<Clock
+				timerDays={timerDays}
+				timerHours={timerHours}
+				timerMinutes={timerMinutes}
+				timerSeconds={timerSeconds}
+			/>
+		</div>
+	);
 }
 
 export default App;
